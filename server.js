e06f74b4084b20a2515bf2c101564bae01246e23
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-03-21 22:28:45 +0530

    Breaking changes - Wrap input and send btn in a form - Fix blank message display error

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const messages = [
   { name: "John Doe", message: "Yes." },
